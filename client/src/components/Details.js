b05623d1e8879b4c2d66af83879cf597961275e4
mbcodes <mbcodes@protonmail.com>
2022-07-04 21:00:06 -0700

    fixed some grid issues

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+
 import { Grid, Typography } from '@mui/material';
 
 const Details = ({ name, address }) => {
--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,18 +1,24 @@
 import { Link } from 'react-router-dom';
 
-import { Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
 
 const NotFound = () => {
   return (
-    <div>
-      <Typography variant='h1'>404 NOT FOUND</Typography>
-      <Typography variant='h2'>
-        You've wondered off the beaten trail...
-      </Typography>
-      <ArrowBackRoundedIcon />
-      <Link to='/'>Back to List</Link>
-    </div>
+    <Grid>
+      <Grid item>
+        <Typography variant='h1'>404 NOT FOUND</Typography>
+        <Typography variant='h2'>
+          You've wondered off the beaten trail...
+        </Typography>
+      </Grid>
+      <Grid item mt={5}>
+        <ArrowBackRoundedIcon />
+        <Link to='/' style={{ color: 'blue' }}>
+          Back to List
+        </Link>
+      </Grid>
+    </Grid>
   );
 };
 

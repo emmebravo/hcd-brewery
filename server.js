95a192672a02fb082e0d2b516921ad99336613cd
mbcodes <mbcodes@protonmail.com>
2022-07-04 20:53:07 -0700

    change repo structure

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import JSONData from './breweries_la.json' assert { type: 'json' };
 
 const app = express();
@@ -16,6 +17,16 @@ app.use(cors());
 app.get('/', (request, response) => {
   response.json(data);
 });
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  //if it hits anything other than the route path
+  app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`server running on PORT ${PORT}`);

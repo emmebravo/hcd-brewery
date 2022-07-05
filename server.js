7fc208ff1271a7abd6c352986de98eedcdbf11ff
mbcodes <mbcodes@protonmail.com>
2022-07-04 21:31:15 -0700

    last updates

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import path from 'path';
 import JSONData from './breweries_la.json' assert { type: 'json' };
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const data = JSONData;
 
 // express middleware
